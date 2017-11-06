fb3cccd547e4197ba6deb802a3a437ff8efb60a3
webProjet Dev Team <dev-team@web-projet.com>
2017-11-06 10:43:16 +0100

    Commit initial

---
+++ _assets/App/_app.js
@@ -0,0 +1,39 @@
+/**
+ * @name _app.js Application Javascript
+ * @author Idea Factory (dev-team@ideafactory.fr) - Oct. 2017
+ * @package _assets\App
+ * @version 0.1
+**/
+
+/**
+ * Mise en place Slick carousel
+**/
+$('#univers-bureau .slide, #univers-sieges .slide').slick({
+	  infinite: true,
+	  dots: true,
+	  slidesToShow: 3,
+	  slidesToScroll: 3
+});
+
+//$('#univers-sieges').removeClass('active').addClass('inactive');
+
+/**
+ * Affichage / Masquage des univers
+**/
+$('.show-univers').on('click', function() {
+	// Récupère l'univers à afficher
+	var univers = $(this).data('rel');
+	//console.log('Univers sélectionné : ' + univers);
+	
+	// Découpe la donnée pour récupérer les éléments du DOM
+	var elements = univers.split('-');
+	var carousel = $('div#' + univers);
+	
+	// Masque l'univers courant
+	var active = $('.univers.active');
+	$(active).removeClass('active').addClass('inactive');
+	
+	// Active l'univers sélectionné
+	carousel.removeClass('inactive').addClass('active');
+	
+});
