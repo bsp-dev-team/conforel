bd60531ef28a0953099f1c826a7382e0b252f247
Benoît Lagrange <benoit.l@bs-partners.fr>
2017-11-07 10:24:59 +0100

    Ajout des univers manquant dans le script JS

--- _assets/App/_app.js
+++ _assets/App/_app.js
@@ -8,7 +8,7 @@
 /**
  * Mise en place Slick carousel
 **/
-$('#univers-bureau .slide, #univers-sieges .slide').slick({
+$('#univers-bureau .slide, #univers-sieges .slide, #univers-enseignement .slide, #univers-ehpad .slide, #univers-restauration .slide ').slick({
 	  infinite: true,
 	  dots: true,
 	  slidesToShow: 3,
@@ -24,16 +24,16 @@ $('.show-univers').on('click', function() {
 	// Récupère l'univers à afficher
 	var univers = $(this).data('rel');
 	//console.log('Univers sélectionné : ' + univers);
-	
+
 	// Découpe la donnée pour récupérer les éléments du DOM
 	var elements = univers.split('-');
 	var carousel = $('div#' + univers);
-	
+
 	// Masque l'univers courant
 	var active = $('.univers.active');
 	$(active).removeClass('active').addClass('inactive');
-	
+
 	// Active l'univers sélectionné
 	carousel.removeClass('inactive').addClass('active');
-	
+
 });
